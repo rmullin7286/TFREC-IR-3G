9cb8694d53ed605f8f592d130245b3b563815c86
Ryan Mullin <ryan.mullin@wsu.edu>
2017-07-06 10:39:07 -0700

    fixed bug in update

--- Pi_Hub/src/main.cpp
+++ Pi_Hub/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-	test();
+	//test();
 	
 	PiHub app;
 	
--- Pi_Hub/update.py
+++ Pi_Hub/update.py
@@ -1,22 +1,20 @@
 import sys
 import os
 
-os.chdir(os.path.dirname(os.path.realpath(__file__)) + '/../')
+filePath = os.path.dirname(os.path.realpath(__file__))
+os.chdir(filePath + '/../')
 os.system('git pull')
 
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.chdir(filePath)
 
 if sys.argv[1] == 'PiSensor':
     os.system('g++ -std=c++11 -g ./src/main.cpp ./src/Adafruit_CharLCD_CPP.cpp ./src/PiSensor.cpp -o PiSensor -lbcm2835 -lrf24 -I/usr/include/python2.7 -lpython2.7')
+    os.system('sudo ./PiSensor')
 
 elif sys.argv[1] == 'PiHub':
     os.system('g++ -std=c++11 -g ./src/main.cpp ./src/PiHub.cpp -o PiHub -lrf24 -lbcm2835')
+    os.system('sudo ./PiHub')
 
-else:
-    os.system('sudo ./Pihub 1')
-    exit(1)
 
-os.system('sudo ./Pihub 0')
-exit(0)
 
 
