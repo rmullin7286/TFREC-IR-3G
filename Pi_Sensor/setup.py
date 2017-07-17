7f7a8297c0cd8df3a76266471f7110f0838fe0ed
rmullin7286 <ryan.mullin@wsu.edu>
2017-07-17 14:18:55 -0700

    working on rf24 bug in setup script, needs testing.

--- Pi_Sensor/setup.py
+++ Pi_Sensor/setup.py
@@ -51,7 +51,6 @@ if not os.WEXITSTATUS(status) == 0:
 
 os.chdir(dependencies + '/RF24')
 os.system('./configure --driver=SPIDEV')
-os.system('sudo make all')
 os.system('sudo make install -B')
 
 #install the libraries for the adafruit char lcd plate
