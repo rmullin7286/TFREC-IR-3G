9dd6b71808120903571d19f05aed4c3730090d99
rmullin7286 <ryan.mullin@wsu.edu>
2017-07-24 12:08:50 -0700

    Added default compile script for developers. Added Auto Log feature. Will test.

--- Pi_Sensor/headers/PiSensor.hpp
+++ Pi_Sensor/headers/PiSensor.hpp
@@ -20,7 +20,7 @@ void test();
 //this is borrowed from the PiHub app for use here.
 enum PacketFlag
 {
-	LOG, UPLOAD, TEST, CONNECT, DISCONNECT, UPDATE
+	LOG, UPLOAD, TEST, CONNECT, DISCONNECT, UPDATE, AUTOLOG
 };
 
 //this is the standard configuration of the packet
@@ -29,15 +29,15 @@ struct Packet
 {
 	//A flag that tells the Pi Hub what type of packet it is.
 	PacketFlag flag;
-	
+
 	//two floats for storing the ambient and object temperatures.
 	float ambient;
 	float object;
-	
+
 	//A signature that the user sets. This is inserted in the excel
 	//file to let the user know what the measurement is.
 	char signature[10];
-	
+
 	//operator overload for packet
 	Packet & operator=(const Packet &rhs)
 	{
@@ -45,7 +45,7 @@ struct Packet
 		this->ambient = rhs.ambient;
 		this->object = rhs.object;
 		strcpy(this->signature, rhs.signature);
-	
+
 		return *this;
 	}
 };
@@ -55,68 +55,75 @@ struct Packet
 class PiSensor
 {
 	// An enum to keep track of which menu option is being displayed or selected.
-	enum MenuItem {CONNECT, TEST, LOG, UPLOAD, SET_SIG, DISCONNECT, SHUTDOWN, UPDATE, BACK};
-	
+	enum MenuItem {CONNECT, TEST, LOG, AUTOLOG, UPLOAD, SET_SIG, DISCONNECT, SHUTDOWN, UPDATE, BACK};
+
 public:
 	//The GPIO pins for the rf24 will automatically be set to 25 and CE0 unless otherwise specified. This default
 	//configuration should work for Raspberry Pi 3.
 	PiSensor(uint16_t gpio=25, uint16_t ce=0);
 	~PiSensor();
-	
+
 	//the main function that runs the whole app. This is initialized once in main
 	//and everything else will be run inside the class.
 	//This function will update the ambient and object temperatures to the screen every second unless the
 	//user holds down the SELECT button. When the user does this they'll be taken to the menu.
 	void run();
-	
+
 	//the function for catching when the program is initialized from the update.py script.
 	void update_finish(int status);
-	
+
 private:
-	
+
 	//This is the function for the menu. The user can use the left and right buttons to scroll through
 	//the different options and select them.
 	void menu(double ambient, double object);
-	
+
 	//This is the main screen run from the run() function that prints the object and ambient to the screen.
 	void mainScreen();
-	
+
 	//These functions are the ones that are run when the user selects the option from the menu.
 	//They each carry out their respective commands.
-	
+
 	//Tell the hub to connect to 3G.
 	void connect();
-	
+
 	//Test whether or not the hub is connected.
 	void test();
-	
+
 	//Send the object and ambient temperature to the hub for logging.
 	void log(double ambient, double object);
-	
+
+	//Logs the files automatically on a timer. Will be uploaded to dropbox once every 15 minutes
+	void autoLogInit();
+
 	//Tell the hub to upload the log files to dropbox.
 	void upload();
-	
+
 	//Allows the user to set a signature so that later they can
 	//tell what they've logged.
 	void setSignature();
-	
+
 	//tell the hub to disconnect from 3G
 	void disconnect();
-	
+
 	//Shuts down the system
 	void shutdown();
-	
+
 	//Runs the update script to update the program to the latest version.
 	void update();
-	
+
 	//go back to the mainScreen() function.
 	void back();
-	
+
 	MLX90614 sensor;
 	LCDShield shield;
 	RF24 radio;
 	string signature;
 	fstream save;
-	
+	bool isAutoLogging;
+	double interval;
+	time_t autoLogStart;
+	time_t autoLogDuration;
+
 	const uint8_t pipes[2][6];
 };
--- Pi_Sensor/src/PiSensor.cpp
+++ Pi_Sensor/src/PiSensor.cpp
@@ -2,6 +2,8 @@
 
 PiSensor::PiSensor(uint16_t gpio, uint16_t ce) : radio(gpio, ce), pipes{"1Node", "2Node"}
 {
+	isAutoLogging = false;
+	interval = 0.0;
 	shield.init_compatible();
 	shield.print("     Hello!");
 	radio.begin();
@@ -13,12 +15,12 @@ PiSensor::PiSensor(uint16_t gpio, uint16_t ce) : radio(gpio, ce), pipes{"1Node",
 	radio.setDataRate(RF24_250KBPS);
 	radio.printDetails();
 	sleep(2);
-	
+
 	save.open("signature.txt", fstream::in);
-	
+
 	if(save.good()) getline(save, signature);
 	else signature = "nosig";
-	
+
 	save.close();
 }
 
@@ -30,23 +32,38 @@ PiSensor::~PiSensor()
 void PiSensor::mainScreen()
 {
 	ostringstream format;
-	
+	double ambient = 0.0, object = 0.0;
+
 	while(1)
 	{
+		if(isAutoLogging)
+		{
+			autoLogDuration = time(NULL);
+			if(difftime(autoLogStart, autoLogDuration) < (interval * 60.0)) log(ambient, object);
+		}
+
 		//reads the temperatures from the sensor.
-		double ambient = sensor.readAmbient();
-		double object = sensor.readObject();
-		
+		ambient = sensor.readAmbient();
+		object = sensor.readObject();
+
 		//check to see if the user has pressed a button.
 		//If any button has been pressed, initiate the menu.
 		if(shield.getButton() != Button::NOBUTTON) menu(ambient, object);
-		
+
+		if(isAutoLogging)
+		{
+			autoLogDuration = time(NULL);
+			if(difftime(autoLogStart, autoLogDuration) < (interval * 60.0)) log(ambient, object);
+			format << std::fixed << "AMBIENT: " << std::setprecision(2) << ambient << "  A\\nOBJECT: " << std::setprecision(2) << object;
+		}
+
 		//If the user isn't holding down a button, the program will continue to print the temperatures to the screen.
-		format << std::fixed << "AMBIENT: " << std::setprecision(2) << ambient << "\\nOBJECT: " << std::setprecision(2) << object;
-		
+		else format << std::fixed << "AMBIENT: " << std::setprecision(2) << ambient << "\\nOBJECT: " << std::setprecision(2) << object;
+
 		shield.print(format.str());
 		format.str("");
-		
+
+
 		//Wait one second between each cycle.
 		sleep(1);
 	}
@@ -54,7 +71,7 @@ void PiSensor::mainScreen()
 
 void PiSensor::menu(double ambient, double object)
 {
-	//CONNECT = 0 
+	//CONNECT = 0
 	//TEST = 1
 	//LOG = 2
 	//UPLOAD = 3
@@ -64,13 +81,19 @@ void PiSensor::menu(double ambient, double object)
 	//UPDATE = 7
 	//BACK = 8
 	Button input;
-	
+
 	//Screen defaults to CONNECT
 	MenuItem item = MenuItem::CONNECT;
 	bool back = false;
-	
-	shield.print("    MENU   ");
-	
+
+	if(isAutoLogging)
+	{
+		shield.print("Auto Log\\nended");
+		isAutoLogging = false;
+	}
+
+	else shield.print("    MENU   ");
+
 	//wait for the user to release the button. This is done so that
 	//CONNECT isn't accidentally selected by holding down too long.
 	do
@@ -78,7 +101,7 @@ void PiSensor::menu(double ambient, double object)
 		input = shield.getButton();
 		usleep(3);
 	}while(input != Button::NOBUTTON);
-	
+
 	while(!back)
 	{
 		//print the current MenuItem onto the screen
@@ -89,6 +112,8 @@ void PiSensor::menu(double ambient, double object)
 		case MenuItem::TEST:       shield.print(" <    TEST    >");
 				break;
 		case MenuItem::LOG:        shield.print(" <    LOG     >");
+				break;
+		case MenuItem::AUTOLOG:    shield.print(" <   AUTOLOG  >");
 				break;
 		case MenuItem::UPLOAD:     shield.print(" <   UPLOAD   >");
 				break;
@@ -103,23 +128,23 @@ void PiSensor::menu(double ambient, double object)
 		case MenuItem::BACK:       shield.print(" <    BACK    >");
 				break;
 		}
-		
+
 		//wait for left or right input
 		do
 		{
 			input = shield.getButton();
 			usleep(3);
-			
+
 		}while(input != Button::LEFT && input != Button::RIGHT && input != Button::SELECT);
-	
+
 		switch(input)
 		{
 			//This case checks if the menu is on CONNECT. If it is, it'll wrap the menu around to BACK. If not, it will decrement the value of the item to scroll left.
 		case Button::LEFT: static_cast<int>(item) == 0 ? item = MenuItem::BACK : item = static_cast<MenuItem>(static_cast<int>(item) - 1);
 			break;
-			
+
 			//Same, but opposite direction.
-		case Button::RIGHT: static_cast<int>(item) == 8 ? item = MenuItem::CONNECT : item = static_cast<MenuItem>(static_cast<int>(item) + 1);
+		case Button::RIGHT: static_cast<int>(item) == 9 ? item = MenuItem::CONNECT : item = static_cast<MenuItem>(static_cast<int>(item) + 1);
 			break;
 		case Button::SELECT:
 			switch(item)
@@ -129,6 +154,8 @@ void PiSensor::menu(double ambient, double object)
 			case MenuItem::TEST: test();
 				break;
 			case MenuItem::LOG: log(ambient, object);
+				break;
+			case MenuItem::AUTOLOG: autoLogInit();
 				break;
 			case MenuItem::UPLOAD: upload();
 				break;
@@ -144,8 +171,12 @@ void PiSensor::menu(double ambient, double object)
 				break;
 			}
 			break;
+		case Button::NOBUTTON:
+		case Button::UP:
+		case Button::DOWN:
+			break;
 		}
-		
+
 		//do another do loop to wait until the user releases the button so that the menu doesn't keep scrolling
 		do
 		{
@@ -161,19 +192,19 @@ void PiSensor::run()
 }
 
 void PiSensor::connect()
-{	
+{
 	Packet send;
 	int status = 0;
 	time_t start, end;
-	
+
 	//prepare the packet
 	send.ambient = 0.0;
 	send.object = 0.0;
 	send.flag = PacketFlag::CONNECT;
 	send.signature[0] = '\0';
-	
+
 	shield.print("Sending\\nrequest");
-	
+
 	//send a packet to the hub requesting a connection.
 	//Repeat 3 times. If it doesn't go through by then, exit the function.
 	for(int i = 0; i < 3; i++)
@@ -183,43 +214,43 @@ void PiSensor::connect()
 			status = 1;
 			break;
 		}
-		
+
 		shield.print("Request not\\nsent. Retrying.");
 	}
-	
+
 	if(status == 0) shield.print("Error: Radio\\ntimeout");
-	
+
 	else
 	{
 		shield.print("Connecting to\\ncell service");
-		
+
 		//wait for success status after hub has connected.
 		start = time(NULL);
 		end = time(NULL);
 		status = -1;
-		
+
 		while(difftime(end, start) < 30.0)
 		{
 			end = time(NULL);
-			
+
 			if(radio.available())
 			{
 				radio.read(&status, sizeof(int));
 				break;
 			}
-			
+
 			usleep(2);
 		}
-		
+
 		radio.stopListening();
-		
+
 		if(status == 1) shield.print("Error: Could not\\nconnect.");
-		
+
 		else if(status == 0) shield.print("Connected\\nsuccesfully!");
-		
+
 		else shield.print("Error: Radio\\ntimeout");
 	}
-	
+
 	sleep(2);
 }
 
@@ -228,13 +259,13 @@ void PiSensor::test()
 	Packet send;
 	int status = 0;
 	time_t start, end;
-	
+
 	//prepare the packet
 	send.ambient = 0.0;
 	send.object = 0.0;
 	send.flag = PacketFlag::TEST;
 	send.signature[0] = '\0';
-	
+
 	//send a packet to the hub requesting a test
 	//Retry 3 times or timeout
 	for(int i = 0; i < 3; i++)
@@ -244,48 +275,48 @@ void PiSensor::test()
 			status = 1;
 			break;
 		}
-		
+
 		shield.print("No response.\\nRetrying.");
 	}
-	
+
 	if(status == 0) shield.print("Error: No radio\\nresponse.");
-	
+
 	else
 	{
 		radio.startListening();
-		
+
 		shield.print("Testing\\nconnection");
-		
+
 		//wait for success status after hub has connected.
 		start = time(NULL);
 		end = time(NULL);
 		status = -1;
-	
-		
-		
+
+
+
 		while(difftime(end, start) < 30.0)
 		{
 			end = time(NULL);
-			
+
 			if(radio.available())
 			{
 				radio.read(&status, sizeof(int));
 				break;
 			}
-			
+
 			usleep(2);
 		}
-		
+
 		radio.stopListening();
-		
+
 		if(status == 1) shield.print("Not connected");
-		
+
 		else if(status == -1) shield.print("Error: radio\\ntimeout.");
-		
+
 		else shield.print("Sensor is\\nconnected!");
-		
+
 	}
-	
+
 	sleep(2);
 }
 
@@ -295,21 +326,21 @@ void PiSensor::log(double ambient, double object)
 	send.flag = PacketFlag::LOG;
 	send.ambient = ambient;
 	send.object = object;
-	
+
 	time_t start;
 	time_t end;
-	
+
 	int status = 0;
-	
+
 	if(signature == "nosig")
 	{
 		setSignature();
 	}
-	
+
 	strcpy(send.signature, signature.c_str());
-	
+
 	shield.print("Sending \\nrequest");
-	
+
 	for(int i = 0; i < 3; i++)
 	{
 		if(radio.write(&send, sizeof(Packet)))
@@ -317,39 +348,39 @@ void PiSensor::log(double ambient, double object)
 			status = 1;
 			break;
 		}
-		
+
 		shield.print("No response.\\nRetrying.");
 	}
-	
+
 	if(!status) shield.print("Error: No radio\\nresponse.");
-	
+
 	else
 	{
 		radio.startListening();
-		
+
 		shield.print("logging\\nmeasurement...");
-		
+
 		start = time(NULL);
 		end = time(NULL);
 		status = 1;
-		
+
 		while(difftime(end, start) < 30.0)
 		{
 			end = time(NULL);
-			
+
 			if(radio.available())
 			{
 				radio.read(&status, sizeof(int));
 				break;
 			}
 		}
-		
+
 		if(status == 1) shield.print("Error: Radio timed out");
 		else shield.print("Log successful");
-		
+
 		radio.stopListening();
 	}
-	
+
 	sleep(2);
 }
 
@@ -359,14 +390,14 @@ void PiSensor::upload()
 	int status = 0;
 	time_t start;
 	time_t end;
-	
+
 	send.flag = PacketFlag::UPLOAD;
 	send.ambient = 0.0;
 	send.object = 0.0;
 	send.signature[0] = '\0';
-	
+
 	shield.print("sending request");
-	
+
 	for(int i = 0; i < 3; i++)
 	{
 		if(radio.write(&send, sizeof(Packet)))
@@ -374,25 +405,25 @@ void PiSensor::upload()
 			status = 1;
 			break;
 		}
-		
+
 		shield.print("No response.\\nRetrying.");
 	}
-	
+
 	if(status == 0) shield.print("Error: Radio\\ntimeout");
-	
+
 	else
 	{
 		radio.startListening();
-		
+
 		shield.print("Request sent.\\nWaiting...");
 		status = 0;
 		start = time(NULL);
 		end = time(NULL);
-		
+
 		while(difftime(end, start) < 60.0)
 		{
 			end = time(NULL);
-			
+
 			if(radio.available())
 			{
 				radio.read(&status, sizeof(int));
@@ -400,13 +431,13 @@ void PiSensor::upload()
 			}
 			usleep(2);
 		}
-		
+
 		if(status == 1) shield.print("Could not\\nupload");
 		else shield.print("Upload\\nsuccesful!");
-		
+
 		radio.stopListening();
 	}
-	
+
 	sleep(2);
 }
 
@@ -414,47 +445,46 @@ void PiSensor::setSignature()
 {
 	Button input;
 	Button release;
-	int cursor;
 	string display;
 	char sig[16] = "\0";
 	bool done = false;
 	int length;
-	
-	
+
+
 	//load the ASCII values into an array; (32 space, 97-122 alphabet, 48-57 numbers)
 	char characters[37];
 	characters[0] = 32;
-	
+
 	for (int i = 1; i < 27; i++)
 	{
 		characters[i] = i + 96;
 	}
-	
+
 	for (int i = 27; i < 37; i++)
 	{
 		characters[i] = i + 21;
 	}
-	
+
 	sig[0] = characters[1];
 	length = 1;
 	display = "Enter signature:\\n";
-	
+
 	while(!done)
 	{
 		shield.print(display + sig);
-		
+
 		do
 		{
 			input = shield.getButton();
 			usleep(2);
 		}while(input == Button::NOBUTTON);
-		
+
 		do
 		{
 			release = shield.getButton();
 			usleep(2);
 		}while(release != Button::NOBUTTON);
-		
+
 		switch(input)
 		{
 		case Button::SELECT: done = true;
@@ -475,18 +505,20 @@ void PiSensor::setSignature()
 			else if(sig[length - 1] == characters[0]) sig[length - 1] = characters[1];
 			else sig[length - 1]++;
 			break;
+		case Button::NOBUTTON:
+			break;
 		}
 	}
-	
+
 	display = "Signature set:\\n";
 	signature = sig;
 	shield.print(display + signature);
-	
+
 	//save the signature to a txt file
 	save.open("signature.txt", fstream::out);
 	save << signature << std::endl;
 	save.close();
-	
+
 	sleep(2);
 }
 
@@ -637,6 +669,51 @@ void PiSensor::update_finish(int status)
 	//exit 1 = already up to date
 	if(status == 0) shield.print("update\\nsuccesful!");
 	else shield.print("Error Could\\nnot update");
-	
+
 	sleep(2);
+}
+
+void PiSensor::autoLogInit()
+{
+	Button button = Button::NOBUTTON;
+	bool end = false;
+	ostringstream format;
+
+	isAutoLogging = true;
+
+	shield.print("beginning\\nauto log");
+	sleep(1);
+
+	while(!end)
+	{
+		format << std::fixed << "Enter time\\n(min): " << std::setprecision(1) << interval;
+		shield.print(format.str());
+		format.str("");
+
+		do
+		{
+			button = shield.getButton();
+		}while(button == Button::NOBUTTON);
+
+		switch(button)
+		{
+		case Button::UP: interval += 0.5;
+			break;
+		case Button::DOWN: interval -= 0.5;
+			break;
+		case Button::SELECT: end = true;
+			break;
+		case Button::NOBUTTON:
+		case Button::LEFT:
+		case Button::RIGHT:
+			break;
+		}
+
+		do
+		{
+			button = shield.getButton();
+		}while(button != Button::NOBUTTON);
+	}
+
+	autoLogStart = autoLogDuration = time(NULL);
 }
